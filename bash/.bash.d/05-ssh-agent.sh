bd88a94e08086e51d52ecada15a58db1d724b336
Eric Hough <eric@tubepress.com>
2019-11-18 07:51:38 -0800

    squash find errors if no ssh-agent socket found

--- bash/.bash.d/05-ssh-agent.sh
+++ bash/.bash.d/05-ssh-agent.sh
@@ -4,7 +4,7 @@
 if [[ -z "$SSH_AUTH_SOCK" && -x /usr/bin/ssh-add ]]; then
 
     # find all possible agent sockets and loop over them most-recent first
-    for candidate in $(find /tmp/ssh-* -type s -name "agent.*" -printf "%T@ %p\n" | sort -rn | cut -d " " -f 2); do
+    for candidate in $(find /tmp/ssh-* -type s -name "agent.*" -printf "%T@ %p\n" 2> /dev/null | sort -rn | cut -d " " -f 2); do
 
         # if this agent has keys, let's use it
         if SSH_AUTH_SOCK="$candidate" /usr/bin/ssh-add -l 2> /dev/null > /dev/null; then
